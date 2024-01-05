82c29b1631f300e250dd603e261b5721afde0d42
Yuriko Maruyama <Yuriko.Maruyama@jp.ricoh.com>
2024-01-05 16:04:12 +0900

    initial commitでゲームがプレイできるファイルだったので上書き

--- src/hurdle.py
+++ src/hurdle.py
@@ -1,22 +1,12 @@
-import pygame
-from image_dict import IMAGEDICT
-from point import Point 
+""" hurdle
 
-class Hurdle:
-    # ハードルが移動する速度
-    speed = 5
+    ハードルのモジュール
+"""
+# ハードル画像を取得するために使用
+# from image_dict import IMAGE_DICT
+# 座標を表現するために使用
+# from point import Point
+# ゲーム画面の幅と高さに依存する定数を
+# from game_settings import WIDTH, HEIGHT
 
-    # ハードルインスタンスの初期化
-    def __init__(self,image_key,scale):  
-        self.image = IMAGEDICT[image_key] # ハードルの画像
-        self.left_top_point = Point(700,300) # 生成位置
-        self.width = self.image.get_width() * scale # 画像の幅
-        self.height = self.image.get_height() * scale # 画像の高さ
-        self.right_bottom_point = Point(self.left_top_point.x + self.width, self.left_top_point.y + self.height) # 画像の右下の座標
-        self.image = pygame.transform.scale(self.image,(self.width,self.height)) # ハードルの描画サイズを修正
-
-    # ハードルを右から左に動かく関数
-    def move(self):
-        # ハードルのxの座標をspeed分減らす
-        self.left_top_point.x -= self.speed
-        self.right_bottom_point.x = self.left_top_point.x + self.width
+# 全部書くぞ！
--- src/point.py
+++ src/point.py
@@ -1,13 +1,11 @@
+"""座標を保持するPointクラスを含むモジュール
+
+x、y座標を保持する。
+x,yをタプルで返すメソッドを持つ。
+"""
 class Point:
-    # プレーヤーの座標を定義する
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    # 座標を文字列で返す
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-    # 座標をタプルで返す
-    def get_xy(self):
-        return (self.x, self.y)
+    """座標を管理する
+    
+    x、y座標を保持する。
+    """
+     
